e0cc486ddeffe01c5505196bbb253ac80e30bdfc
Ashley-P <ashleyp1244@hotmail.co.uk>
2019-04-06 14:02:11 +0100

    Very basic proof of concept about printing valid html to a file

--- src/main.c
+++ src/main.c
@@ -1,3 +1,22 @@
+#include <stdio.h>
+
+
+
 int main(int argc, char** argv) {
-    return 0;
+    FILE *fp = _wfopen(L".\\htmltest.html", L"w");
+
+    fprintf(fp,
+            "<!DOCTYPE html>\n"
+            "<html>\n"
+            "<head>\n"
+            "<title>TEST</title>\n"
+            "</head>\n"
+            "<body>\n"
+            "<h1>Header 1</h1>\n"
+            "<p>This is a paragraph</p>\n"
+            "</body>\n"
+            "</html>\n"
+           );
+
+    fclose(fp);
 }
